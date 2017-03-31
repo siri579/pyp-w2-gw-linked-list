7ba7514c3d52ff9ce15f0a2f9c711bf7bc2b24f0
siri579 <siri579@gmail.com>
2017-03-30 22:22:43 -0500

    Update node.py

--- linked_list/node.py
+++ linked_list/node.py
@@ -4,13 +4,15 @@ class Node(object):
     """
 
     def __init__(self, elem, next=None):
-        pass
+        self.elem = elem
+        self.next = next
 
     def __str__(self):
-        pass
+        return 'Node({})'.format(self.elem) + ' > ' + ('Node({})'.format(self.next.elem) if self.next else '/')
+                
 
     def __eq__(self, other):
-        pass
+        return self.elem == other.elem and self.next == other.next if self and other else False
 
     def __repr__(self):
-        pass
+        return str(self)
